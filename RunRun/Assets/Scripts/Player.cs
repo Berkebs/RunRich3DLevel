0bbb199eea2d7d3193414d274769407c8ebdc57d
Berke Burak SARP <berkets61@windowslive.com>
2022-04-13 11:06:17 +0300

    Up

--- RunRun/Assets/Scripts/Player.cs
+++ RunRun/Assets/Scripts/Player.cs
@@ -2,20 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using Lean.Touch;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Touch touch;
+    public float horizontalspeed,verticalspeed;
+    CharacterController cc;
+    Vector3 move = Vector3.zero;
+
+    //        verticalspeed = 15;
+    //        horizontalspeed = 0.003f;
+
+
     void Start()
     {
         DOTween.Init();
+
+
+        cc = this.gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount>0)
+        {
+
+            touch = Input.GetTouch(0);
+
+            if (touch.phase==TouchPhase.Moved)
+            {
+                move.x = transform.position.x + touch.deltaPosition.x;
+                
+                cc.Move(move * horizontalspeed);
+            }
+
+        }
+        cc.Move(Vector3.forward * Time.deltaTime * verticalspeed);
     }
    
 }

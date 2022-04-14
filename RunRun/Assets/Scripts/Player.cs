8c498ae26e28e6ef07aeaf41ce90bd3e000d11cc
Berke Burak SARP <berkets61@windowslive.com>
2022-04-14 03:01:10 +0300

    Up

--- RunRun/Assets/Scripts/Player.cs
+++ RunRun/Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using TMPro;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -19,13 +20,16 @@ public class Player : MonoBehaviour
     public int[] ReqMoney;
     public string[] LevelName;
 
-    public TextMeshPro LevelNametxt;
-   
-
+    public TextMeshProUGUI LevelNametxt;
+    public Image MoneyImage;
+    Vector3 Charactergo;
     void Start()
     {
         DOTween.Init();
         cc = this.gameObject.GetComponent<CharacterController>();
+        MoneyImage.fillAmount = (float)Money/ (float)ReqMoney[Level];
+        LevelNametxt.text = LevelName[Level];
+        Charactergo = Vector3.forward;
     }
 
     // Update is called once per frame
@@ -38,15 +42,41 @@ public class Player : MonoBehaviour
 
             if (touch.phase==TouchPhase.Moved)
             {
-                move.x = transform.position.x + touch.deltaPosition.x;
-                
+
+                if (Charactergo==Vector3.forward|| Charactergo == Vector3.back)
+                {
+                    move.x = touch.deltaPosition.x;
+                }
+                else
+                {
+                    move.z = -touch.deltaPosition.x;
+                }
+
                 cc.Move(move * horizontalspeed);
             }
 
         }
-        cc.Move(Vector3.forward * Time.deltaTime * verticalspeed);
+        cc.Move(Charactergo * Time.deltaTime * verticalspeed);
     }
 
+    void ForwardController(float rotate) 
+    {
+        switch (rotate)
+        {
+            case 0:
+                Charactergo = Vector3.forward;
+                break;
+            case 90:
+                Charactergo = Vector3.right;
+                break;
+            case -90:
+                Charactergo = Vector3.left;
+                break;
+            case -180:
+                Charactergo = Vector3.back;
+                break;
+        }
+    }
 
     void PickingObject(Objectcs go) 
     {
@@ -60,6 +90,8 @@ public class Player : MonoBehaviour
             LevelDown(ReqMoney[Level]-Money);
         }
         Debug.Log("Level : " + Level + ",  Money : " + Money);
+        MoneyImage.fillAmount = (float)Money / (float)ReqMoney[Level];
+        LevelNametxt.text = LevelName[Level];
     }
 
 
@@ -75,8 +107,6 @@ public class Player : MonoBehaviour
         {
             Debug.Log("MaxLevel");
         }
-
-        LevelNametxt.text = LevelName[Level];
     }
     void LevelDown(int excessmoney) {
         Debug.Log("Level Down  Level : " + Level + " Money : " + Money);
@@ -89,8 +119,6 @@ public class Player : MonoBehaviour
         {
             Debug.Log("GameOver");
         }
-
-        LevelNametxt.text = LevelName[Level];
     }
 
     private void OnTriggerEnter(Collider other)
@@ -98,6 +126,16 @@ public class Player : MonoBehaviour
         if (other.tag=="Object")
         {
             PickingObject(other.gameObject.GetComponent<Objectcs>());
+        }
+        else if (other.gameObject.name=="RightCol")
+        {
+            ForwardController(transform.rotation.y + 90);
+            transform.DOLocalRotate(new Vector3(transform.rotation.x,transform.rotation.y+90,transform.rotation.z),1);
+        }
+        else if (other.gameObject.name=="LeftCol")
+        {
+            ForwardController(transform.rotation.y - 90);
+            transform.DOLocalRotate(new Vector3(transform.rotation.x, transform.rotation.y - 90, transform.rotation.z), 1);
         }
     }
 
